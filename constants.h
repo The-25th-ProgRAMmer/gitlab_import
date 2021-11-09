69628eb8a7814dd4c97b00c007c5e3d53afd2bbd
robvasistha <78962360+robvasistha@users.noreply.github.com>
2021-11-09 20:37:16 +0000

    Added constants to this new file

---
+++ constants.h
@@ -0,0 +1,9 @@
+const int RELAY      = 2;        // Relay Pin to turn the pump on set to D2
+const int BUZZER     = 7;        // Buzzer set to D7
+//const int MOISTURE_SENSOR = A0;  // Moisture Sensor set to A0
+
+const int LED_STRIP = 6;         //LED STRIP on and off  
+const int TEMP_SENS = 5;         //temperature pin set to D5
+
+//const int WEIGHT_CLK = A4;        //CLK Pin and Data out pin to the nanoboard. 
+//const int WEIGHT_DOUT = A5; 
