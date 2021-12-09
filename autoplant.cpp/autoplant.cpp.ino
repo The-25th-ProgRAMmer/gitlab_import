f0c9e675593663e264513a6aa7b6e3b55821c52f
RobertV <robvasistha@gmail.com>
2021-12-09 16:44:17 +0000

    Display code added

--- autoplant.cpp/autoplant.cpp.ino
+++ autoplant.cpp/autoplant.cpp.ino
@@ -1,4 +1,8 @@
-#include <Wire.h> 
+#include <Wire.h>
+#include <SPI.h>
+#include "epd2in9.h"
+#include "epdpaint.h"
+#include "imagedata.h" 
 #include "DS1307.h" //oorrect DC1307 grove lobrary 
 #include "dht.h" 
 #include "HX711.h" 
@@ -12,15 +16,26 @@ HX711 scale = HX711();           //weight object name changed.
 
 //describes the battery object. 
 
+unsigned long currentTime(0);
+unsigned long lastPrintToDisplay(0);
+
+unsigned char image[1024];
+Paint paint(image, 0, 0);    // width should be the multiple of 8 
+Epd epd;
+
 //placeholder figure to calibrate sensor (has potential to cause bugs)
 float calibration_factor = -83886.00;
 
 int moistValue = 430;              // read moisture value 
-int battery_value = 0;            //read the battery level. 
+int battery_value = 100;            //read the battery level. 
 
 int dryLimit    = 720;      //most of these, the user should be able to set in app
 int waterLimit  = 420;       
-int EnclosureTemp = 18;     
+int EnclosureTemp = 18;
+int wifi = 0;
+float weight = 0;
+float temperature = 0;
+int humidity = 0;     
 float WeightValue = 1;
 int wateringTick = 0;
 int tempTick = 0;
@@ -63,11 +78,30 @@ void setup(){
   scale.set_scale(calibration_factor);
   scale.tare();   //reset scale to 0 assuming no weight. 
   
+  if (epd.Init(lut_full_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+  }
+
+  epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayFrame();
+  epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.DisplayFrame();
+
+  if (epd.Init(lut_partial_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+  }
 }
 
 void loop(){
  //digitalWrite(PUMP,LOW);
- 
+ currentTime = millis();
+
+  if (currentTime - lastPrintToDisplay >= DISPLAY_UPDATE_INTERVAL) {
+    Display();
+    lastPrintToDisplay = currentTime;
+  }
  //Display();              
  //TempCheck();          
  WeightCheck();          
@@ -77,7 +111,6 @@ void loop(){
  //WaterPump();
 //digitalWrite(BUZZER,HIGH);
 //digitalWrite(BUZZER,LOW);
-delay(200);
 }
 
 //added semicolons where it needed to be done. 
@@ -144,7 +177,9 @@ void LEDCheck(){
 }
 void TempCheck(){
   //checks the temperature
-  if(trunc(dht.readTemperature()) > EnclosureTemp)
+  temperature = trunc(dht.readTemperature());
+  humidity = dht.readHumidity();
+  if(temperature > EnclosureTemp)
   //tag = 3;
   {
     
@@ -169,7 +204,7 @@ void TempCheck(){
 
 void WeightCheck(){
   //checks the weight of the water canister.
-  float weight = scale.get_units();
+  weight = scale.get_units();
   Serial.print("weight= ");
   Serial.print(weight);
   Serial.print("\n");
@@ -218,8 +253,95 @@ void BatteryLevel(){
 }
 
 void Display(){
-  //E-ink to be written by Rogier
-  //display time and date by default via RTC
+    //296 x 128
+
+  paint.SetRotate(ROTATE_90);
+
+  
+  paint.SetWidth(10);
+  paint.SetHeight(296);
+
+  paint.Clear(UNCOLORED);
+  char line1String[30];
+  
+  if (wifi == 0)
+    sprintf(line1String, "WiFi: Not Connected");
+  else
+    sprintf(line1String, "WiFi: Connected");
+    
+  paint.DrawStringAt(2, 2, line1String, &Font12, COLORED);
+  char line2String[30];
+  sprintf(line2String, "Battery Level: %d%%", batteryLevel);
+  paint.DrawStringAt(162, 2, line2String, &Font12, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 118, 0, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  char line3String[30];
+  sprintf(line3String, "Moisture Level:  %d%%", moisture);
+  paint.DrawStringAt(2, 5, line3String, &Font12, COLORED);
+  char line4String[30];
+  sprintf(line4String, "Temperature: %dC", temperature);
+  paint.DrawStringAt(182, 5, line4String, &Font12, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 20, 0, paint.GetWidth(), paint.GetHeight());
+
+  paint.Clear(UNCOLORED);
+  char line5String[30];
+  sprintf(line5String, "Reservoir Level: %d%%", waterLevel);
+  paint.DrawStringAt(2, 5, line5String, &Font12, COLORED);
+  char line6String[30];
+  sprintf(line6String, "Humidity:    %d%%", humidity);
+  paint.DrawStringAt(182, 5, line6String, &Font12, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
+
+
+  paint.SetWidth(16);
+  paint.SetHeight(94);
+
+  paint.Clear(UNCOLORED);
+  char line7String[30];
+  //clock.getTime()
+  //sprintf(line7String, "%d:%d:%d", clock.hour, clock.minute, clock.second);
+  sprintf(line7String, "15:58:34");
+  paint.DrawStringAt(2, 2, line7String, &Font16, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 100, 101, paint.GetWidth(), paint.GetHeight());
+
+
+  paint.SetWidth(20);
+  paint.SetHeight(296);
+
+  paint.Clear(UNCOLORED);
+  char line8String[30];
+  sprintf(line8String, "Autonomous Incubator");
+  paint.DrawStringAt(8, 10, line8String, &Font20, COLORED);
+  epd.SetFrameMemory(paint.GetImage(), 56, 0, paint.GetWidth(), paint.GetHeight());
+
+
+  if (batteryLevel <= 20) {
+    paint.SetWidth(20);
+    paint.SetHeight(242);
+  
+    paint.Clear(UNCOLORED);
+    char line9String[30];
+    sprintf(line9String, "Battery Level Low");
+    paint.DrawStringAt(2, 4, line9String, &Font20, COLORED);
+    epd.SetFrameMemory(paint.GetImage(), 76, 27, paint.GetWidth(), paint.GetHeight());
+  }
+
+
+  if (waterLevel <= 20) {
+    paint.SetWidth(20);
+    paint.SetHeight(270);
+  
+    paint.Clear(UNCOLORED);
+    char line10String[30];
+    sprintf(line10String, "Water Reservoir Low");
+    paint.DrawStringAt(2, 8, line10String, &Font20, COLORED);
+    epd.SetFrameMemory(paint.GetImage(), 36, 13, paint.GetWidth(), paint.GetHeight());
+  }
+
+
+  epd.DisplayFrame();
+  epd.ClearFrameMemory(0xFF);
 }
 
 //Code programmed by Anthony A
